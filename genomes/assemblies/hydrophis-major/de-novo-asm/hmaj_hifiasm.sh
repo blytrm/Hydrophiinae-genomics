391e5dda12318d7a317512b25205119b8263051f
Billy Trim <trimgoldsmith@gmail.com>
2026-02-13 11:25:35 +1030

    Update hifiasm script with new file paths and options

--- genomes/assemblies/hydrophis-major/de-novo-asm/hmaj_hifiasm.sh
+++ genomes/assemblies/hydrophis-major/de-novo-asm/hmaj_hifiasm.sh
@@ -2,35 +2,38 @@
 #SBATCH --job-name=hifiasm_hydmaj
 #SBATCH --nodes=1
 #SBATCH --cpus-per-task=64
-#SBATCH --mem=750G
+#SBATCH --mem=240G
 #SBATCH --time=3-00:00:00
-#SBATCH -p highmem
+#SBATCH -p icelake
 #SBATCH --output=hifiasm_%j.out
 #SBATCH --error=hifiasm_%j.err
 #SBATCH --mail-user=13billy.trim13@gmail.com
 #SBATCH --mail-type=ALL
 
 module purge
+module load Anaconda3
+source $(conda info --base)/etc/profile.d/conda.sh
+conda activate /hpcfs/users/a1864358/miniconda/envs/hifiasm
+
 cd $SLURM_SUBMIT_DIR
 
-HIFI="/uofaresstor/sanders_lab/projects/billy/genome-assembly/seq/public/hydrophis_major/hifi/hydmaj.fastq.gz"
-HIC_R1="/uofaresstor/sanders_lab/projects/billy/genome-assembly/seq/public/hydrophis_major/hic-trim/350845_R1.fastq.gz"
-HIC_R2="/uofaresstor/sanders_lab/projects/billy/genome-assembly/seq/public/hydrophis_major/hic-trim/350845_R2.fastq.gz"
+HIFI="/hpcfs/users/a1864358/sanders_lab/asm/files/hmaj/hifiasm/hydmaj.fastq.gz"
+HIC_R1="/hpcfs/users/a1864358/sanders_lab/asm/files/hmaj/hifiasm/350845_R1.fastq.gz"
+HIC_R2="/hpcfs/users/a1864358/sanders_lab/asm/files/hmaj/hifiasm/350845_R2.fastq.gz"
 OUTDIR="/hpcfs/users/a1864358/sanders_lab/asm/files/hmaj/hifiasm"
 mkdir -p $OUTDIR
 cd $OUTDIR
 
-rm -f hydmaj.fastq.gz 350845_R1.fastq.gz 350845_R2.fastq.gz
-ln -s $HIFI hydmaj.fastq.gz
-ln -s $HIC_R1 350845_R1.fastq.gz
-ln -s $HIC_R2 350845_R2.fastq.gz
+ls -la hydmaj.fastq.gz 350845_R1.fastq.gz 350845_R2.fastq.gz
+file hydmaj.fastq.gz
 
-hifiasm \\
-    -o hmaj_26 \\
-    -t ${SLURM_CPUS_PER_TASK} \\
-    --primary \\
-    --dual_scaf \\
-    --hom-cov 30 \\
-    -u 1 \\
-    --h1 350845_R1.fastq.gz --h2 350845_R2.fastq.gz \\
+hifiasm \
+    -o hmaj_26 \
+    -t ${SLURM_CPUS_PER_TASK} \
+    --primary \
+    --dual-scaf \
+    --hom-cov 30 \
+    -u 1 \
+    --h1 350845_R1.fastq.gz \
+    --h2 350845_R2.fastq.gz \
     hydmaj.fastq.gz

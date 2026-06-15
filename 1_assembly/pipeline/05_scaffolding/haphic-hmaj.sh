02ebdafa891f64c54a4ddbcd49d34b67459fbb64
Billy Trim <trimgoldsmith@gmail.com>
2026-06-16 03:02:37 +0930

    Create haphic-hmaj.sh

---
+++ 1_assembly/pipeline/05_scaffolding/haphic-hmaj.sh
@@ -0,0 +1,41 @@
+#!/usr/bin/env bash
+#SBATCH --job-name=haphic-hmaj
+#SBATCH -p icelake
+#SBATCH -N 1
+#SBATCH --cpus-per-task=48
+#SBATCH --time=24:00:00
+#SBATCH --mem=240GB
+#SBATCH -o %x_%j.out
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=13billy.trim13@gmail.com
+
+# haphic for hydrophis major
+
+module purge
+
+export PATH="/scratchdata1/users/a1864358/miniconda/bin:$PATH"
+source $(conda info --base)/etc/profile.d/conda.sh
+conda activate /hpcfs/users/a1864358/miniconda/envs/haphic
+
+cd /hpcfs/users/a1864358/sanders_lab/asm/files/hmaj/hifiasm
+
+bwa index hmaj_filt_denovo.fa
+
+bwa mem -5SP -t ${SLURM_CPUS_PER_TASK} hmaj_filt_denovo.fa ./350845_R1.fastq.gz ./350845_R2.fastq.gz | samblaster | samtools view - -@ 14 -S -h -b -F 3340 -o HiC-hmaj.bam
+
+/hpcfs/users/a1864358/sanders_lab/asm/files/hmaj/hifiasm/HapHiC/utils/filter_bam HiC-hmaj.bam 1 --nm 3 --threads 20 | samtools view - -b -@ 14 -o HiC-hmaj.filtered.bam
+
+FA="/hpcfs/users/a1864358/sanders_lab/asm/files/hmaj/hifiasm/hmaj_filt_denovo.fa"
+BAM="/hpcfs/users/a1864358/sanders_lab/asm/files/hmaj/hifiasm/HiC-hmaj.filtered.bam"
+OUT="/hpcfs/users/a1864358/sanders_lab/asm/files/hmaj/hifiasm/haphic"
+SW="/hpcfs/users/a1864358/sanders_lab/asm/files/hmaj/hifiasm/HapHiC"
+
+${SW}/haphic pipeline \
+    ${FA} ${BAM} 15 \
+    --RE "GATC,GANTC,CTNAG,TTAAC" \
+    --outdir ${OUT}/n15 \
+    --threads ${SLURM_CPUS_PER_TASK} \
+    --processes ${SLURM_CPUS_PER_TASK} \
+    --correct_nrounds 4 \
+    --npop 200 \
+    --mutprob 0.15
